4396e2a0b756a2a7333ce4542d016c92c585d5c2
Radek Bigaj <r.bigaj@fibcode.com>
2020-06-24 20:26:18 +0200

    printing name and data of last distillation

--- src/App.js
+++ src/App.js
@@ -3,36 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 
-
-function getSessionsData() {
-  const sessions_url = `http://127.0.0.1:8000/sessions`
-  axios.get(sessions_url).then(response => {
-    if (response.status === 200) {
-      console.log(response.data);
-      return response.data[0]
-    }
-  })
+class App extends React.Component {
+  state= {
+    sessionName: "",
+    sessionDate: ""
+  }
+  distillation_date;
+  componentDidMount() {
+    axios
+        .get("http://127.0.0.1:8000/sessions")
+        .then(response => this.setState({sessionName: response.data[0].name,
+          sessionDate: response.data[0].distillation_date}))
+  }
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+              {this.state.sessionName}
+            </p>
+            <p>
+              {this.state.sessionDate}
+            </p>
+          </header>
+        </div>
+    );
+  }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {getSessionsData()}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
 export default App;
